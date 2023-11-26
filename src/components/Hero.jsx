437e228bcdd9d2600bb3eef0cd2d5cab23640b3a
Arjo Ghosh <122384138+ArjoGhosh12@users.noreply.github.com>
2023-11-27 00:22:13 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -126,13 +126,13 @@ const Hero = () => {
       <Navbar />
       <Container>
         <Left>
-          <Title>Think. Make. Solve.</Title>
+          <Title>...Think. Make. Solve......</Title>
           <WhatWeDo>
             <Line src="./img/line.png" />
             <Subtitle>What we Do</Subtitle>
           </WhatWeDo>
           <Desc>
-            we enjoy creating delightful, human-centered digital experiences.
+            .....We enjoy creating delightful,      human-centered digital experiences.
           </Desc>
           <Button>Learn More</Button>
         </Left>
